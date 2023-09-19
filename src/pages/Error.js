7ccd0c31d1a7bafe7785b66c058d7e69b8155af6
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-19 14:06:35 +0400

    Router Setup

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return <div>error page</div>;
 };
 const Wrapper = styled.section`
   min-height: 100vh;
