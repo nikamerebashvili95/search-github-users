07ed5c96a493c04de5442cd41a786ad4029498bf
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-19 14:18:41 +0400

    work with Error Page, Login Page and add Context

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GithubProvider>
+      <App />
+    </GithubProvider>
   </React.StrictMode>
 );
