50226bafc77f681b005814e3ebfd518553504a9b
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-20 15:57:45 +0400

    Calculate Most Used

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,25 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 import { useContext } from "react";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, []);
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
   const chartData = [
     {
       label: "HTML",
@@ -23,7 +41,7 @@ const Repos = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData} />
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   );
