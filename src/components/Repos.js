8e873dd34c10d4f18d3868ee3768387cc94aa1a5
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-21 15:56:53 +0400

    add Column and Bar Charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -54,7 +54,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
+        <Column3D data={chartData} />
         <Doughnut2D data={mostPopular} />
+        <Bar3D data={chartData} />
       </Wrapper>
     </section>
   );
