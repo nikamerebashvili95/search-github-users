25b36cfadb12346f5d52f64d59f9d37f55207380
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-22 14:37:31 +0400

    add Error

--- src/context/context.js
+++ src/context/context.js
@@ -15,6 +15,8 @@ const GithubProvider = ({ children }) => {
   //request loading
   const [requests, setRequests] = useState(0);
   const [loading, setIsLoading] = useState(false);
+  //error
+  const [error, setError] = useState({ show: false, msg: "" });
   //check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -24,15 +26,20 @@ const GithubProvider = ({ children }) => {
         } = data;
         setRequests(remaining);
         if (remaining === 0) {
-          //throw an error
+          toggleError(true, "sorry, you have exceeded your horly rate limit!");
         }
       })
       .catch((err) => console.log(err));
   };
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
   //error
   useEffect(checkRequests, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
