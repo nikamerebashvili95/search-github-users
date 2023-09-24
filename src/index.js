a60f40360aca5b7c9dcf78768bd3f1836e8e873f
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-24 15:33:35 +0400

    add Auth0

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+// dev-r4bfsfo87xmrp4yo.us.auth0.com
+// UN6YXPevx7jpuVyJKaIeCR06e83qvQdP
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-r4bfsfo87xmrp4yo.us.auth0.com"
+      clientId="UN6YXPevx7jpuVyJKaIeCR06e83qvQdP"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
